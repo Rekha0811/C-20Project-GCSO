1214607f773e7ce7918a4725ec2229c7e64448e4
Rekha0811 <71653503+Rekha0811@users.noreply.github.com>
2020-09-29 12:29:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var car,wall,speed,weight, Def;
+
+
+function setup() {
+  
+  createCanvas(1600,400);
+  car = createSprite(50,200,50,50);
+  wall = createSprite(1000,200,60,height/2);
+car.shapeColor = "blue";
+  wall.shapeColor = "orange";
+  speed = Math.round(random(55,90));
+  weight = Math.round(random(400,1500));
+  console.log("Speed is "+speed);
+  console.log("Weight is "+weight);
+}
+
+function draw() {
+  background(255,255,255);  
+  if(keyDown("space"))
+  {
+    car.velocityX = speed;
+  }
+
+if(car.collide(wall))
+{
+  Def = (0.5*weight*speed*speed)/22500;
+  console.log("Deformation is " + Def);
+}
+if(Def<100)
+{
+  car.shapeColor = "Green";
+  console.log("It's Green");
+}
+if(Def>100 && Def <180)
+{
+  car.shapeColor = "Yellow";
+  console.log("It's Yellow");
+}
+if(Def > 180)
+{
+  car.shapeColor = "red";
+  console.log("It's Red");
+}
+  
+  drawSprites();
+}
